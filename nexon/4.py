3d4520b459760e8702f13def24f36534082f2f55
Jaehyun LEE <aprilmagnificat@gmail.com>
2023-07-28 09:41:20 +0900

    Delete nexon directory

--- nexon/4.py
+++
@@ -1,9 +0,0 @@
-def getkthSmallestTerm(arr, k):
-    from itertools import product
-    
-    r = list(product(arr, repeat=2))
-    r.sort()
-    return [r[k-1][0], r[k-1][1]]
-
-
-print(getkthSmallestTerm([2,2,1], 5))
